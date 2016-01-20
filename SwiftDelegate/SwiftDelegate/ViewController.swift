40bdcab96c11bacdbcacc39b38d4ebda61f5a4af
ixx <ixxcome123@gamil.com>
2016-01-20 17:13:59 +0800

    init

---
+++ SwiftDelegate/SwiftDelegate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDelegate
+//
+//  Created by apple on 16/1/20.
+//  Copyright © 2016年 www.ixx.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
